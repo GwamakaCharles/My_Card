55cae3d84dbe7f6b15d17d64cc6ee0aad6759b58
Gwamaka Charles <gwaxart19@gmail.com>
2020-07-30 21:49:42 +0300

    my not final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/pied-piper.png'),
+              ),
+              Text(
+                'Gwamaka C. Mwamwaja',
+                style: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
