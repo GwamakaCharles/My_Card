f3b81d76079e6b02eeef69f824088dd3e017cdc1
Gwamaka Charles <gwaxart19@gmail.com>
2020-07-30 21:52:19 +0300

    added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -19,7 +20,61 @@ class MyApp extends StatelessWidget {
               ),
               Text(
                 'Gwamaka C. Mwamwaja',
-                style: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold),
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'iOS DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17.0,
+                    letterSpacing: 5.5,
+                    color: Colors.teal.shade100),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '+255 772 634 954',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal.shade900),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'gmwamwaja@outlook.com',
+                    style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0),
+                  ),
+                ),
               ),
             ],
           ),
